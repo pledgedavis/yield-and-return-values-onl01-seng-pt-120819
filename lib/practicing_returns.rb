cd22eec343a75de3dd9386240b5ed9048b3d2676
pledgedavis <pledgedavis14@gmail.com>
2019-12-20 18:10:07 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -12,11 +12,12 @@
 
 def hello(array)
   i = 0
+  collection = []
   while i < array.length
-    binding.pry
-    yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
+  collection
 end
 
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
