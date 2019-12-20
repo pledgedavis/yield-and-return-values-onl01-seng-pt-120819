3199e2e45cd37d4a51c8c4445f079dfb2e982b0a
pledgedavis <pledgedavis14@gmail.com>
2019-12-20 18:10:49 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,38 @@
-require 'pry'
+ require 'pry'
+
+# def hello(array)
+#   i = 0
+#   while i < array.length
+#     yield(array[i])
+#     i += 1
+#   end
+# end
+
+# hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
 
 def hello(array)
   i = 0
+  collection = []
   while i < array.length
-    yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
+  collection
 end
 
-
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
